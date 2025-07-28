43a02b35473112b892ae329381094a71c1bc1cae
Aalyan Raza <aalyanrkazmi@gmail.com>
2025-07-28 17:37:51 +0500

    Mongo DB connected

--- final-project/api/src/index.ts
+++ final-project/api/src/index.ts
@@ -2,12 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { sendMagicLink } from './auth';
+import { connectToMongo } from './lib/mongo';
+import { User } from './models/User';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+connectToMongo();
 
 // Test route
 app.get('/', (req, res) => {
@@ -32,3 +36,25 @@ app.post('/api/send-magic-link', async (req, res) => {
     res.status(400).json({ success: false, error: error.message });
   }
 });
+
+app.get('/', (_req, res) => {
+  res.send('API running');
+});
+
+app.listen(4000, () => {
+  console.log('Server running on http://localhost:4000');
+});
+
+// test route to add a user
+app.post('/api/test-create-user', async (req, res) => {
+  try {
+    const user = await User.create({
+      email: 'test@example.com',
+      name: 'Test User'
+    });
+
+    res.json({ message: 'User created', user });
+  } catch (err) {
+    res.status(500).json({ error: 'Error creating user', details: err });
+  }
+}); 
